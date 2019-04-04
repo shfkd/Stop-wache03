ecaccba53d490d02ae50e68982bcb3472804b11c
D7703_07 <d7703_07@lulzsec.local>
2019-04-04 12:42:07 +0900

    dsd

--- Stop wache03/ViewController.swift
+++ Stop wache03/ViewController.swift
@@ -16,32 +16,56 @@ class ViewController: UIViewController {
     var myTimer = Timer()
     var timer_running = false
     
+    @IBOutlet weak var playBtn: UIBarButtonItem!
+    @IBOutlet weak var pauseBtn: UIBarButtonItem!
+    @IBOutlet weak var stopBtn: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        timeLbl.text = String(count)
+        timeLbl.text = "00:00:00"
     }
 
-    @IBAction func playBtn(_ sender: UIBarButtonItem) {
+    @IBAction func playBtn(_ sender: Any) {
         myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
         timer_running = true
         
-    }
-    
-    @IBAction func pauseBtn(_ sender: UIBarButtonItem) {
-        myTimer.invalidate()
+        changeButtonState(play: false, pause: true, stop: true)
         
     }
     
-    @IBAction func stopBtn(_ sender: UIBarButtonItem) {
+    @IBAction func pauseBtn(_ sender: Any) {
+        myTimer.invalidate()
+        
+        changeButtonState(play: true , pause: false, stop: true)
+        
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
         myTimer.invalidate()
         count = 0
-        timeLbl.text = String(count)
+        timeLbl.text = "00:00:00"
+        
+        changeButtonState(play: true, pause: true, stop: false)
+        
     }
+    
     
     @objc func updateTimer(){
         count += 1
-        timeLbl.text = String(count)
+        let min = count / 6000
+        let sec = count / 100 - (min*60)
+        let msec = count % 100
+        let output = String(format: "%02d:%02d:%02d", min, sec, msec)
+        timeLbl.text = output
+    }
+    
+    func changeButtonState(play: Bool, pause: Bool, stop: Bool){
+        playBtn.isEnabled = play
+        stopBtn.isEnabled = stop
+        pauseBtn.isEnabled = pause
+        
     }
 }
 
